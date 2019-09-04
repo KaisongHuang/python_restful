2f18d5f8f44902cabc48e23a3016715d3783f73d
Ubuntu <k86huang@uwaterloo.ca>
2019-09-04 15:58:37 +0000

    implement some APIs

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,23 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
 	return 'Hello world!'
+
+
+@app.route('/hello')
+def hello():
+	return 'hello'
+
+
+@app.route('/world')
+def world():
+	return 'world'
+
 
 if __name__ == '__main__':
 	app.run()
